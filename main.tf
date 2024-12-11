3fe2dacf072b0c7de8390fe35ce36b8e79d28c31
Mazin2k2 <mazin.sunjofy@gmail.com>
2024-12-11 17:19:40 +0530

    Update

--- main.tf
+++ main.tf
@@ -11,18 +11,18 @@ resource "azurerm_virtual_network" "main" {
 }
 
 resource "azurerm_subnet" "public" {
-  for_each            = toset(var.public_subnets)
-  name                = "public-${split("/", each.key)[1]}"
-  address_prefixes    = [each.key]
-  resource_group_name = azurerm_resource_group.main.name
+  for_each             = toset(var.public_subnets)
+  name                 = "public-${split("/", each.key)[1]}"
+  address_prefixes     = [each.key]
+  resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
 }
 
 resource "azurerm_subnet" "private" {
-  for_each            = toset(var.private_subnets)
-  name                = "private-${split("/", each.key)[1]}"
-  address_prefixes    = [each.key]
-  resource_group_name = azurerm_resource_group.main.name
+  for_each             = toset(var.private_subnets)
+  name                 = "private-${split("/", each.key)[1]}"
+  address_prefixes     = [each.key]
+  resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
 }
 
@@ -33,9 +33,9 @@ resource "azurerm_kubernetes_cluster" "main" {
   dns_prefix          = "vprofile"
 
   default_node_pool {
-    name       = "default"
-    vm_size    = "Standard_DS2_v2"
-    node_count = 2
+    name           = "default"
+    vm_size        = "Standard_DS2_v2"
+    node_count     = 2
     vnet_subnet_id = azurerm_subnet.private["172.20.1.0/24"].id
   }
 
--- versions.tf
+++ versions.tf
@@ -8,14 +8,15 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstate"
+    storage_account_name = "tfstate0909"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
 
-  required_version = "~> 1.6.3"
+  required_version = "~> 1.10.1"
 }
 
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
 }
